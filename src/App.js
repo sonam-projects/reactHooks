7f77083d83fc28c5866fdc1f0b972ed5e471abd3
sonam-projects <60036106+sonam-projects@users.noreply.github.com>
2020-01-21 23:10:42 +0530

    simple try

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./styles.css";
+const MyContext = React.createContext();
 
-export default function App() {
+const Input = () => {
+  const changeFirstName = e => {
+    console.log(e);
+  };
+  const contextData = useContext(MyContext);
+  const {
+    user: { firstName, lastName }
+  } = contextData;
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h5>{firstName + " " + lastName}</h5>
+      <input
+        onChange={e => {
+          changeFirstName = e.target.value;
+        }}
+      />
     </div>
   );
+};
+const FormInput = () => {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+};
+class App extends React.Component {
+  state = {
+    user: { firstName: "Sonam", lastName: "Deolalikar" },
+    age: 26,
+    height: 154
+  };
+  render() {
+    const changeFirstName = () => {
+      this.setState({ [this.state.user.firstName]: "Sudhir" });
+    };
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <MyContext.Provider value={{ changeFirstName, ...this.state }}>
+          <FormInput />
+        </MyContext.Provider>
+      </div>
+    );
+  }
 }
+export default App;
